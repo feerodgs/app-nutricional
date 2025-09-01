3929766d33d6bc36fa77644482ee737a2ce491eb
Felipe Julhão <felipeaj220@gmail.com>
2025-08-31 23:47:18 -0300

    First commit

---
+++ backend/src/main/java/br/com/nutriprojeto/nutri_ai/NutriAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutriprojeto.nutri_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriAiApplication.class, args);
+	}
+
+}
